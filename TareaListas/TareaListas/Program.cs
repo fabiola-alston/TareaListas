1ca95521593e2b0ad430c8b356a3f7ad5aadbd26
fabiola-alston <f.alston.1@estudiantec.cr>
2024-09-09 23:43:37 -0600

    Project creation and IList interface

---
+++ TareaListas/TareaListas/Program.cs
@@ -0,0 +1,38 @@
+﻿#nullable disable
+
+// nota: nombre la interfaz "ILista" en vez de "IList" como en los requerimientos por que al parecer C# tiene una interfaz/clase o algo(? llamado IList ya.
+public interface ILista
+{
+    public void InsertInOrder();
+    public int DeleteFirst();
+    public int DeleteLast();
+    public bool DeleteValue();
+    public int GetMiddle();
+    public void MergeSorted();
+
+}
+
+public class DoubleNode
+{
+    public int value;
+    public DoubleNode next;
+    public DoubleNode previous;
+
+
+}
+
+public class DoublyLinkedList : ILista
+{
+    private DoubleNode first;
+
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        DoublyLinkedList doubleList = new DoublyLinkedList();
+
+    }
+}
